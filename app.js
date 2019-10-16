2b23a2a9ff87f0938827a43847d07160694e2d45
kierepkae <kierepkaedyta@gmail.com>
2019-10-16 19:54:20 +0100

    add test.html

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (request, response) => {
   response.send('Hello Codebar');
 })
