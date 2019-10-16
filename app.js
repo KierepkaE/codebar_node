0212ea0c6b56dbd8cad9d9aa68c349059cce9b24
kierepkae <kierepkaedyta@gmail.com>
2019-10-16 20:15:48 +0100

    build server and define path route for form

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+
+app.use(express.urlencoded());
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-  response.send('Hello Codebar');
+  response.sendFile(__dirname + '/index.html');
+});
+app.post('/tasks', function (req, res) {
+  console.log(req.body);
+  fs.writeFileSync("b.json", req.params);
+  res.send('add a NEW TASK');
 })
 
 
